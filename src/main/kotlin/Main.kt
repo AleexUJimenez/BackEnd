12dc71c57125b2592e55b267bf91b8f8e20fe49d
ALEX JIMENEZ ROQUE <98989398+AleexUJimenez@users.noreply.github.com>
2023-05-18 12:42:46 -0600

    avance 5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,17 +4,23 @@ import factorymethod.OracleDBAdapter
 import properties.PropertiesUtil
 import java.util.*
 
-suspend fun main() {
+fun main() {
+    /*
+        val property: Properties? = PropertiesUtil
+            .loadProperty("properties/DBMySQL")
+        println(property)
+        val mySql: IDBAdapter = MySqlDBAdapter()
+        println(mySql.getConnection().toString())
 
-    val property: Properties? = PropertiesUtil
-        .loadProperty("properties/DBMySQL")
-    println(property)
-    val mySql: IDBAdapter = MySqlDBAdapter()
-    println(mySql.getConnection().toString())
+        val oracle: IDBAdapter = OracleDBAdapter()
+        println(oracle.getConnection().toString())
 
-    val oracle: IDBAdapter = OracleDBAdapter()
-    println(oracle.getConnection().toString())
+    */
+    val projects = listOf(
+        "Command",
+        "Chain of Responsability")
 
-
-
+    println(projects[Random().nextInt(0,projects.size-1)])
 }
+
+
